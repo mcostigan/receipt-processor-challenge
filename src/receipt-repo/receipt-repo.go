5fcd73ca8098cde4c958d38afd7584e0f9258f6e
Matt Costigan <costigan@uchicago.edu>
2023-05-25 21:25:44 -0500

    make inmemory store thread safe

--- src/receipt-repo/receipt-repo.go
+++ src/receipt-repo/receipt-repo.go
@@ -3,6 +3,7 @@ package receipt_repo
 import (
 	"fmt"
 	"receipt-processor-challeng/src/model"
+	"sync"
 )
 
 type ReceiptRepo interface {
@@ -26,10 +27,14 @@ func (err *NoReceiptFoundError) Error() string {
 	return fmt.Sprintf("Receipt with id %s does not exist", err.Id)
 }
 
+var mutex = &sync.RWMutex{}
+
 // Get Searches the data store for a receipt with a given id and returns.
 // Throws an exception if no such id exists
 func (repo *inMemoryReceiptRepo) Get(id string) (*model.Receipt, error) {
+	mutex.Lock()
 	receipt, ok := repo.receipts[id]
+	mutex.Unlock()
 
 	if !ok {
 		return nil, &NoReceiptFoundError{id}
@@ -41,6 +46,8 @@ func (repo *inMemoryReceiptRepo) Get(id string) (*model.Receipt, error) {
 
 // Set Saves an instance of receipt, keyed on its id
 func (repo *inMemoryReceiptRepo) Set(receipt *model.Receipt) *model.Receipt {
+	mutex.Lock()
 	repo.receipts[receipt.Id] = receipt
+	mutex.Unlock()
 	return receipt
 }
