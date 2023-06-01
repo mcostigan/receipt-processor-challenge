548b4b8a84d4011890fa86871d917e814f971e9f
Matt Costigan <costigan@uchicago.edu>
2023-05-31 23:05:21 -0500

    add integration tests

--- src/rules/rules-service.go
+++ src/rules/rules-service.go
@@ -2,6 +2,7 @@ package rules
 
 import (
 	"receipt-processor-challeng/src/model"
+	"sync"
 )
 
 type ServiceInterface interface {
@@ -26,8 +27,21 @@ func NewRulesService() ServiceInterface {
 // PointReceipt Find the total points earned on the receipt /**
 func (service *service) PointReceipt(receipt *model.Receipt) int {
 	points := 0
+	pointsLock := sync.Mutex{}
+
+	var wg sync.WaitGroup
 	for _, rule := range service.rules {
-		points += rule.evaluate(receipt)
+		// evaluate each rule concurrently and update the total points
+		wg.Add(1)
+		go func(receipt *model.Receipt, rule Rule) {
+			pointsLock.Lock()
+			points += rule.evaluate(receipt)
+			pointsLock.Unlock()
+			wg.Done()
+		}(receipt, rule)
 	}
+
+	// wait for the evaluation of all rules
+	wg.Wait()
 	return points
 }
